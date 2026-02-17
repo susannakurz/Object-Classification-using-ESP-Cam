f3d80f5939bc30bcfc0159f8593e8cbea99e3fa7
Susanna <57864168+susannakurz@users.noreply.github.com>
2026-02-17 13:11:26 +0100

    Update setup_linux.sh

--- src/linuxSetup/setup_linux.sh
+++ src/linuxSetup/setup_linux.sh
@@ -1,2 +1,5 @@
-wget https://files.pythonhosted.org/packages/5e/cd/4b95ef7f293b927c283db0b136c42be91c8ec6845c44de0238c8c23bdc80/torch-2.10.0-cp314-cp314-manylinux_2_28_x86_64.whl
-pip install torch-2.10.0-cp314-cp314-manylinux_2_28_x86_64.whl
+pip install --upgrade pip
+pip install -r .venv/requirements_class.txt
+mkdir tmp_pip
+TMPDIR=tmp_pip/ pip install --cache-dir=$TMPDIR -r ./venv/requirements_torch.txt
+echo "fertig! :)"
