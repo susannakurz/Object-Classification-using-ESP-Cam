f49abc63a4736ea423f7159885ca5a6e137771ab
Susanna <57864168+susannakurz@users.noreply.github.com>
2026-02-17 15:03:54 +0100

    Add files via upload

--- src/linuxSetup/setup_linux.sh
+++ src/linuxSetup/setup_linux.sh
@@ -3,11 +3,11 @@ ls
 echo "[INFO] entered .venv/"
 pip install --upgrade pip
 echo "[INFO] upgraded pip"
-pip install -r "requirements_class.txt"
+pip install -r "requirements_class.txt" 
 echo "[INFO] non-torch libraries installed"
 mkdir tmp_pip
 echo "[INFO] tmp_pip cache-folder created"
-TMPDIR=tmp_pip/ pip install --cache-dir=$TMPDIR -r "requirements_torch.txt"
+TMPDIR=tmp_pip/ pip install --cache-dir=$TMPDIR -r "requirements_torch.txt" 
 echo "[INFO] torch libraries installed"
 rm -r tmp_pip
 echo "[INFO] deleted tmp cache"
