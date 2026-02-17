84a6eeb3333680b3bca477d4edaa8f0032cfec6d
Susanna <57864168+susannakurz@users.noreply.github.com>
2026-02-17 11:25:53 +0100

    Add files via upload

---
+++ src/linuxSetup/setup_linux.sh
@@ -0,0 +1,2 @@
+wget https://files.pythonhosted.org/packages/5e/cd/4b95ef7f293b927c283db0b136c42be91c8ec6845c44de0238c8c23bdc80/torch-2.10.0-cp314-cp314-manylinux_2_28_x86_64.whl
+pip install torch-2.10.0-cp314-cp314-manylinux_2_28_x86_64.whl
